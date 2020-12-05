32e913078b2c1de57677324c6497bf0f770cedf7
msunkist99 <msunkist99@gmail.com>
2020-12-04 20:02:43 -0600

    Chapter 13 Model Validation Studio -- works but not right yet

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -4,7 +4,10 @@ import org.launchcode.spaday.data.UserData;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -12,13 +15,18 @@ public class UserController {
 
     // handles GET requests to /user/add
     @GetMapping("add")
-    public String displayAddUserForm(){
+    public String displayAddUserForm(Model model){
+        model.addAttribute(new User());
         return "user/add";
     }
 
     // handles POST requests to /user/add
     @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors, String verify, Model model) {
+        if(errors.hasErrors()) {
+            return "user/add";
+        }
+
         if(user.getPassword().equals(verify)){
             UserData.add(user);
             model.addAttribute("users", UserData.getAll());
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,17 +1,30 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class User {
+    @NotBlank(message="User name cannot be blank or null")
     private String username;
+
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @Size(min=6, message="Password must be at least 6 characters.")
     private String password;
     private int id;
     private String joinDate;
 
     private static int nextId = 0;
 
+    // noArg controller
+    public User(){}
+
+    // controller
     public User(String username, String email, String password) {
         String dateFormat = "mm-dd-yyyy";
         SimpleDateFormat simpleDateForm = new SimpleDateFormat(dateFormat);
